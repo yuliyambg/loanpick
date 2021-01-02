0fb460447b722803ba8b722ef730078ac800d748
yuliyambg <yuliyambg@gmail.com>
2021-01-02 13:11:22 -0500

    add scope to load model; update views/loans/index

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,11 @@
 class Loan < ApplicationRecord
   belongs_to :borrower
   belongs_to :lender
+
+  scope :student, -> { where(category: "Student") }
+  scope :hil, -> { where(category: "HIL") }
+  scope :consolidation, -> { where(category: "Consolidation") }
+  scope :fixed, -> { where(term: "120 months fixed") }
+  scope :variable, -> { where(term: "180 months variable")}
+
 end
