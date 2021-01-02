c58a2460d85c655958d37018c7c49fd9546ce5b7
yuliyambg <yuliyambg@gmail.com>
2021-01-02 11:27:51 -0500

    add borrowers controller crud

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -1,3 +1,25 @@
 class BorrowersController < ApplicationController
 
+  def new
+    @borrower = Borrower.new
+  end
+
+  def index
+    @borrowers = Borrower.all
+  end
+
+  def create
+    @borrower = Borrower.new(borrower_params)
+  end
+
+  def show
+    @borrower = Borrower.find_by(id: params[:id])
+    @borrower.save
+  end
+
+  private
+
+  def borrower_params
+    params.require(:borrower).permit(:first_name, :last_name, :dob, :street_address, :city, :zipcode, :email, :password, :password_confirmation)
+  end
 end
